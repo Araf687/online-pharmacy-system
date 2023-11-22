b7d49a4f13fe872ccaaa9038c0338bb683cbbd3b
araf687 <arafyasin@gmail.com>
2023-11-23 00:15:13 +0600

    ok

--- includes/navbar.php
+++ includes/navbar.php
@@ -32,9 +32,7 @@ if (isset($_SESSION['userType']) == false) {
             <a href="category.php" class="link-underline-light text-decoration-none me-3">Categories</a>
             <a href="all-shop.php" class="link-underline-light text-decoration-none me-3">Shops</a>
             <a href="" class="link-underline-light text-decoration-none me-3">Help</a>
-<<<<<<< HEAD
-            <a href="login.php" class="link-underline-light text-decoration-none"><i class="fa fa-sign-in"></i> login</a>
-=======
+
             <span class="position-relative">
                 <span><i class="fa-solid fa-cart-shopping" style="font-size:20px"></i></span>
 
@@ -57,9 +55,6 @@ if (isset($_SESSION['userType']) == false) {
                 <a href="login.php" onclick="logout()" class="link-underline-light text-decoration-none"><i
                         class="fa-solid fa-arrow-right-from-bracket"></i> Logout</a>
             <?php } ?>
-
-
->>>>>>> 249bb7af2c956c3f0c6be2d078be9e0273367e97
         </nav>
     </div>
     <script>
