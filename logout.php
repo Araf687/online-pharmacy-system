5ad73b4fdb1d3a497c83fb201978297dcbc45c63
araf687 <arafyeasin@gmail.com>
2024-01-18 18:54:51 +0600

    ok

--- logout.php
+++ logout.php
@@ -1,6 +1,11 @@
 <?php
 session_start();
-session_destroy();
+unset($_SESSION['userType']);
+unset($_SESSION['user_name']);
+unset($_SESSION['user_img']);
+unset($_SESSION['loggedInId']);
+unset($_SESSION['userLatitude']);
+unset($_SESSION['userLongitude']);
 echo "<script>console.log('asas');localStorage.removeItem('loggedInData');</script>";
 header("Location: index.php");
 ?>
